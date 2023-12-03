7cb70edf2d293e4d52750eed575d16131f71e5f1
Ruteski <lruteski@gmail.com>
2023-12-03 20:51:18 -0300

    varias coisas

--- prog-basica-avancada-mp/PrimeiroCodigo/Tarefa03.cpp
+++ prog-basica-avancada-mp/PrimeiroCodigo/Tarefa03.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
-
+#include <iomanip> // para mostrar o double completo
 
 int main() {
 	setlocale(LC_ALL, "portuguese");
 
-	std::cout << "Eu" << std::endl;
-	std::cout << "vou" << std::endl;
-	std::cout << "aprender \n\c++\n\n" << std::endl;
+	int NumVidas = 5;
+	double numero = 452325.954213;
+
+	std::cout 
+		<< "Eu" << std::endl
+	    << "vou" << std::endl
+	    << "aprender" << "\n" 
+		<< "c++" << "\n"
+		<< "Vidas do jogador : " << NumVidas << "\n"
+		<< "Endereço que NumVidas ocupa na memória RAM: " << &NumVidas << "\n"
+		<< "tamanho da variavel: " << sizeof(NumVidas) << "Bytes" << "\n"
+		<< "***********************************************************\n"
+		<< "valor do numero double: " << std::setprecision(12) << numero << "\n"
+		<< "Endereço que NumVidas ocupa na memória RAM: " << &numero << "\n"
+		<< "tamanho da variavel: " << sizeof(numero) << "Bytes" << "\n"
+		<< std::endl;
+
 	std::cout << "Código em c++!" << std::endl;
 	//std::cout << "C++" << std::endl;
 
-	system("PAUSE");
-	return 0;
+	//return 0;
 }
