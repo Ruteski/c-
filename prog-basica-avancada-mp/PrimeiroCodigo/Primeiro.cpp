f3f4c5fbb237eeef5e62232d9e590e71f190e522
Ruteski <lruteski@gmail.com>
2023-12-03 14:33:01 -0300

    inicio

---
+++ prog-basica-avancada-mp/PrimeiroCodigo/Primeiro.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+//int main() {
+//	std::cout << "Primeiro codigo! Hello world!" << std::endl;
+//	system("color 8B");
+//	system("PAUSE");
+//	return 0;
+//}
---
+++ prog-basica-avancada-mp/PrimeiroCodigo/Tarefa03.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+int main() {
+	setlocale(LC_ALL, "portuguese");
+
+	std::cout << "Eu" << std::endl;
+	std::cout << "vou" << std::endl;
+	std::cout << "aprender \n\c++\n\n" << std::endl;
+	std::cout << "Código em c++!" << std::endl;
+	//std::cout << "C++" << std::endl;
+
+	system("PAUSE");
+	return 0;
+}
